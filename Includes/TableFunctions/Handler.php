c0b20dfbb9cf825d9c11ea464062e1d257da88ff
root <pkasemer@gmail.com>
2024-12-15 18:50:30 +0000

    user login and registeration

--- Includes/TableFunctions/Handler.php
+++ Includes/TableFunctions/Handler.php
@@ -102,6 +102,119 @@ class Handler
         }
     }
 
+    public function getMwonyaPasswordResetTokenByToken($token){
+        try {
+            // Use a prepared statement to prevent SQL injection
+            $query = "SELECT * FROM MwonyaPasswordResetToken  WHERE token = ? LIMIT 1";
+            $stmt = $this->conn->prepare($query);
+            if (!$stmt) {
+                throw new Exception("Failed to prepare statement: " . $this->conn->error);
+            }
+
+            $stmt->bind_param("s", $token); 
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $user = $result->fetch_assoc();
+
+            $stmt->close();
+            return $user; // Return user as associative array
+        } catch (Exception $e) {
+            error_log("Database error in MwonyaPasswordResetToken: " . $e->getMessage());
+            return null;
+        }
+    }
+
+    public function getMwonyaPasswordResetTokenByEmail($email){
+        try {
+            // Use a prepared statement to prevent SQL injection
+            $query = "SELECT * FROM MwonyaPasswordResetToken  WHERE email = ? LIMIT 1";
+            $stmt = $this->conn->prepare($query);
+            if (!$stmt) {
+                throw new Exception("Failed to prepare statement: " . $this->conn->error);
+            }
+
+            $stmt->bind_param("s", $email); 
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $user = $result->fetch_assoc();
+
+            $stmt->close();
+            return $user; // Return user as associative array
+        } catch (Exception $e) {
+            error_log("Database error in MwonyaPasswordResetToken: " . $e->getMessage());
+            return null;
+        }
+    }
+
+    public function createVerificationToken($data){
+        #get the  email, token, expires
+        $email = $data['email'];
+        $token = $data['token'];
+        $expires = $data['expires'];
+
+        try {
+              // Prepare the insert query
+            $insert_query = "INSERT INTO MwonyaVerificationToken (email, token, expires) 
+            VALUES (?, ?, ?)";
+
+            $insert_stmt = $this->conn->prepare($insert_query);
+            if (!$insert_stmt) {
+            throw new Exception("Failed to prepare insert statement: " . $this->conn->error);
+            }
+
+            // Bind parameters to the insert query
+            $insert_stmt->bind_param("sss", $email, $token, $expires);
+
+            // Execute the query
+            $insert_stmt->execute();
+            $insert_stmt->close();
+
+            // Return success response
+            return [
+            'status' => 'success',
+            'message' => 'Verification Token saved successfully.'
+            ];
+
+        } catch (Exception $e){
+            error_log("Database error in VerificationToken: " . $e->getMessage());
+            return $e->getMessage();
+        }
+    }
+
+    public function createPasswordResetToken($data){
+        #get the  email, token, expires
+        $email = $data['email'];
+        $token = $data['token'];
+        $expires = $data['expires'];
+        try {
+              // Prepare the insert query
+            $insert_query = "INSERT INTO MwonyaPasswordResetToken (email, token, expires) 
+            VALUES (?, ?, ?)";
+
+            $insert_stmt = $this->conn->prepare($insert_query);
+            if (!$insert_stmt) {
+            throw new Exception("Failed to prepare insert statement: " . $this->conn->error);
+            }
+
+            // Bind parameters to the insert query
+            $insert_stmt->bind_param("sss", $email, $token, $expires);
+
+            // Execute the query
+            $insert_stmt->execute();
+            $insert_stmt->close();
+
+            // Return success response
+            return [
+            'status' => 'success',
+            'message' => 'PasswordReset Token saved successfully.'
+            ];
+
+        } catch (Exception $e){
+            error_log("Database error in PasswordReset: " . $e->getMessage());
+            return $e->getMessage();
+        }
+    }
+
 
     public function registerMwonyaCreator($data){
         $email = $data['email'];
@@ -136,6 +249,66 @@ class Handler
         } catch (Exception $e){
             error_log("Database error in getMwonyaCreatorByEmail: " . $e->getMessage());
             return $e->getMessage();
+        }
+    }
+
+    public function deleteTokenbyID($token_id){
+        // Prepare the delete query
+        $query = "DELETE FROM MwonyaVerificationToken WHERE id =  ?";
+        
+        // Prepare the statement
+        $stmt = $this->conn->prepare($query);
+        if (!$stmt) {
+            throw new Exception("Failed to prepare statement: " . $this->conn->error);
+        }
+        
+        // Bind the current date parameter
+        $stmt->bind_param("s", $token_id);
+
+        // Execute the query
+        if ($stmt->execute()) {
+            $deleted_rows = $stmt->affected_rows; // Get the number of deleted rows
+            $stmt->close();
+            return [
+                'status' => 'success',
+                'message' => "$deleted_rows tokens deleted successfully."
+            ];
+        } else {
+            $stmt->close();
+            return [
+                'status' => 'error',
+                'message' => "Failed to delete token with id: $token_id."
+            ];
+        }
+    }
+
+    public function deletePasswordResetTokenbyID($id){
+        // Prepare the delete query
+        $query = "DELETE FROM MwonyaPasswordResetToken WHERE id =  ?";
+        
+        // Prepare the statement
+        $stmt = $this->conn->prepare($query);
+        if (!$stmt) {
+            throw new Exception("Failed to prepare statement: " . $this->conn->error);
+        }
+        
+        // Bind the current date parameter
+        $stmt->bind_param("s", $id);
+
+        // Execute the query
+        if ($stmt->execute()) {
+            $deleted_rows = $stmt->affected_rows; // Get the number of deleted rows
+            $stmt->close();
+            return [
+                'status' => 'success',
+                'message' => "$deleted_rows tokens deleted successfully."
+            ];
+        } else {
+            $stmt->close();
+            return [
+                'status' => 'error',
+                'message' => "Failed to delete token with id: $id."
+            ];
         }
     }
 
---
+++ Requests/endpoints/deletePasswordResetTokenbyID.php
@@ -0,0 +1,64 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate"); // HTTP 1.1
+header("Pragma: no-cache"); // HTTP 1.0
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once 'includedFiles.php';
+
+if (!isset($db) || empty($db)) {
+    http_response_code(500);
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'Database connection not established.',
+    ]);
+    exit;
+}
+
+// Parse input JSON
+$data = json_decode(file_get_contents("php://input"), true);
+
+if (!isset($data) || (!isset($data['id']))) {
+    http_response_code(400);
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'Provide a valid token id',
+    ]);
+    exit;
+}
+
+try {
+    // Handler setup
+    $handler = new Handler($db);
+
+    // Get user based on provided input
+    $result = null;
+    $result = $handler->deletePasswordResetTokenbyID($data['id']);
+
+    // Respond based on result
+    if ($result) {
+        http_response_code(200);
+        echo json_encode([
+            'status' => 'success',
+            'data' => $result,
+        ]);
+    } else {
+        http_response_code(404);
+        echo json_encode([
+            'status' => 'error',
+            'message' => 'Error Found.',
+        ]);
+    }
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'An error occurred: ' . $e->getMessage(),
+    ]);
+}
+?>
